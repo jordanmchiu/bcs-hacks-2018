04e0d617aee52d6dce0f79efe03cb1f17ec541b3
timpel <timpellissier@gmail.com>
2018-03-10 17:37:55 -0800

    Integrated RouteManager and ApptManager

--- src/ApptManager.js
+++ src/ApptManager.js
@@ -1,8 +1,8 @@
 // import * as parse5 from "/../node_modules/parse5/lib/index";
-
+let RouteManager = require("./RouteManager");
 
 class HTMLParser {
-    routify(inputObject) {
+    async routify(inputObject) {
         let that = this;
         let rp = require('request-promise');
         let parse5 = require('parse5');
@@ -24,14 +24,17 @@ class HTMLParser {
                 });
             entries.push(routificEntry);
         }
-        Promise.all(entries).then((finishedEntires) => {
+        Promise.all(entries).then(async (finishedEntries) => {
             let toRoutific = {
                 numFriends: inputObject["drivers"],
                 startLoc: "2350 Wesbrook Mall",
                 endLoc: "2366 Main Mall",
-                appointments: finishedEntires,
+                appointments: finishedEntries,
             };
-            return toRoutific;
+            const rm = new RouteManager();
+            const routificRes = await rm.sendRequest(toRoutific);
+            console.log(JSON.stringify(routificRes));
+            return routificRes;
         }).catch((err) => {
             console.log("Promise.all error");
         })
@@ -66,7 +69,9 @@ class HTMLParser {
                 lat: latlng["lat"],
                 lng: latlng["lng"],
             },
+            duration: listing["duration"],
             start: listing["time"],
+            end: listing["endtime"],
             priority: listing["priority"],
         };
     }
@@ -80,14 +85,63 @@ testObject = {
             name: "Posh 4 Bedroom",
             url: 'https://vancouver.craigslist.ca/van/apa/d/posh-4-bedroom-35-bathroom/6514756192.html',
             time: '12:00',
-            priority: 'High',
+            endtime: '13:00',
+            duration: 30,
+            priority: 'high',
         },
         {
             name: "Garden Suite on Main",
             url: 'https://vancouver.craigslist.ca/van/apa/d/1-bedroom-garden-suite-steps/6520226947.html',
             time: '13:00',
-            priority: 'High',
+            endtime: '14:00',
+            duration: 30,
+            priority: 'high',
+        },
+        {
+            name: "Sweet apartment",
+            url: 'https://vancouver.craigslist.ca/nvn/apa/d/home-sweet-home/6526163160.html',
+            time: '13:00',
+            endtime: '14:00',
+            duration: 30,
+            priority: 'high',
+        },
+        {
+            name: "Townhouse in Burnaby",
+            url: 'https://vancouver.craigslist.ca/van/roo/d/sfu-fic-douglas-college/6526179287.html',
+            time: '11:00',
+            endtime: '12:00',
+            duration: 30,
+            priority: 'high',
+        },
+        {
+            name: "Basement Suite 4th",
+            url: 'https://vancouver.craigslist.ca/nvn/apa/d/large-furnished-studio/6526168419.html',
+            time: '13:30',
+            endtime: '14:30',
+            duration: 30,
+            priority: 'high',
+        },
+        {
+            name: "Rowhouse",
+            url: 'https://vancouver.craigslist.ca/rds/apa/d/1-bedroom-basement-for-rent/6526149064.html',
+            time: '14:30',
+            endtime: '15:30',
+            duration: 30,
+            priority: 'high',
         }
+
     ],
 }
 testParser.routify(testObject);
+
+
+
+    /*
+
+                https://vancouver.craigslist.ca/van/apa/d/lovely-3-bedroom-family-home/6526135165.html
+                    https://vancouver.craigslist.ca/van/apa/d/2-bedroom-ground-lvl-suite/6526119554.html
+                        https://vancouver.craigslist.ca/van/vac/d/maui-westin-ocean-front-villa/6516979321.html
+                            https://vancouver.craigslist.ca/van/apa/d/fully-furnished-1-bedroom/6516464710.html
+                                https://vancouver.craigslist.ca/van/apa/d/2-bed-in-kitsilano-for-rent/6514867233.html
+                                    https://vancouver.craigslist.ca/van/apa/d/perfect-bright-2-level-3/6526045992.html
+                                    */
